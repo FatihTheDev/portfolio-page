4a0b457ff7385b9a75cd46a4e8305d492f28f9b5
FatihTheDev <fatihtheg123@gmail.com>
2026-08-14 12:57:40 +0200

    Fixed responsiveness of the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-full bg-neutral-50 dark:bg-neutral-900 transition-colors duration-300">
-      <div className="hero-section grid gap-8 md:grid-cols-2 items-center max-w-7xl mx-auto px-4 py-12 md:py-24">
+      <div className="hero-section grid gap-8 grid-cols-1 md:grid-cols-2 items-center max-w-5xl mx-auto px-6 py-8 xl:py-16">
         <div className="flex justify-center">
           <Image
             src={pfp}
             alt="business headshot"
-            className="h-[200px] w-[200px] md:h-[400px] md:w-[400px] object-cover rounded-full shadow-xl border-4 border-white dark:border-neutral-800 dark:shadow-neutral-950"
+            className="h-[150px] w-[150px] md:h-[400px] md:w-[400px] object-cover rounded-full shadow-xl border-4 border-white dark:border-neutral-800 dark:shadow-neutral-950"
           />
         </div>
 
@@ -30,9 +30,9 @@ export default function Home() {
                 1000,
                 `I am Fatih Mekić, an aspiring frontend web developer with knowledge in HTML, CSS, JavaScript, React, Bootstrap, TailwindCSS, C, C++, Kotlin, Python, MongoDB and Neo4J. I am currently polishing my React knowledge and am ready to code anytime.\nI also have experience in efficiently prompting various large language models to drastically boost my workflow.\n`,
                 1000,
-                `I am Fatih Mekić, an aspiring frontend web developer with knowledge in HTML, CSS, JavaScript, React, Bootstrap, TailwindCSS, C, C++, Kotlin, Python, MongoDB and Neo4J. I am currently polishing my React knowledge and am ready to code anytime.\nI also have experience in efficiently prompting various large language models to drastically boost my workflow.\nI am a big linux enthusiast and am a proud daily driver of Arch Linux, as it allows me to learn a lot about Linux processes and the system itself.\nI have a positive attitude towards learning new things and am always see`,
+                `I am Fatih Mekić, an aspiring frontend web developer with knowledge in HTML, CSS, JavaScript, React, Bootstrap, TailwindCSS, C, C++, Kotlin, Python, MongoDB and Neo4J. I am currently polishing my React knowledge and am ready to code anytime.\nI also have experience in efficiently prompting various large language models to drastically boost my workflow.\nI am a big Linux enthusiast and am a proud daily driver of Arch Linux, as it allows me to learn a lot about Linux processes and the system itself.\nI have a positive attitude towards learning new things and am always see`,
                 1000,
-                `I am Fatih Mekić, an aspiring frontend web developer with knowledge in HTML, CSS, JavaScript, React, Bootstrap, TailwindCSS, C, C++, Kotlin, Python, MongoDB and Neo4J. I am currently polishing my React knowledge and am ready to code anytime.\nI also have experience in efficiently prompting various large language models to drastically boost my workflow.\nI am a big linux enthusiast and am a proud daily driver of Arch Linux, as it allows me to learn a lot about Linux processes and the system itself.\nI have a positive attitude towards learning new things and am always seeking new`,
+                `I am Fatih Mekić, an aspiring frontend web developer with knowledge in HTML, CSS, JavaScript, React, Bootstrap, TailwindCSS, C, C++, Kotlin, Python, MongoDB and Neo4J. I am currently polishing my React knowledge and am ready to code anytime.\nI also have experience in efficiently prompting various large language models to drastically boost my workflow.\nI am a big Linux enthusiast and am a proud daily driver of Arch Linux, as it allows me to learn a lot about Linux processes and the system itself.\nI have a positive attitude towards learning new things and am always seeking new`,
                 1000,
                 () => setShowTyped(true),
               ]}
